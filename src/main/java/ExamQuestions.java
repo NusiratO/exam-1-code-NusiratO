5362501ee5d349fb7d68450368146d29ecd49672
Nusi Olumegbon <nolumegbon@ithaca.edu>
2020-10-22 20:00:04 -0400

    Added to removeThirdItem for an array

--- src/main/java/ExamQuestions.java
+++ src/main/java/ExamQuestions.java
@@ -1,16 +1,54 @@
 import java.util.List;
 
+import org.graalvm.compiler.hotspot.stubs.OutOfBoundsExceptionStub;
+
 public class ExamQuestions {
 
     public static int getMiddleItem(List<Integer> aList){
-        return -1;
+        if(aList.size() == 0){
+            throw new IllegalArgumentException();
+      }
+        int middleItem = aList.size();
+        middleItem = middleItem/2;
+        return aList.get(middleItem);
+
+
     }
 
     public static void removeThirdItem(String[] anArray){
-        
+        if (anArray.length < 3){
+            throw new IllegalArgumentException(); 
+        }
+        else if(anArray[2] == null){
+            throw new IllegalArgumentException();
+        }
+            int index = 2; 
+            for (int i = 2; i < anArray.length - 1; i++){
+                if(anArray[i+1] == null){
+                    anArray[i] = null;
+                }
+                else{
+                    anArray[i] = anArray[i + 1];
+                }
+            }
+            anArray[anArray.length-1] = null;     
     }
 
     public static void removeThirdItem(Node firstNode){
-        
+        Node current = firstNode;
+        int count = 1;
+        if(firstNode.getItem() == null){
+            throw new IllegalArgumentException();
+        }
+        if(firstNode.getNext() == null){
+            firstNode.setNext(null);
+        }
+        while(current.getNext() != null || count != 3){
+            count++;
+            current = current.getNext();
+        }
+
+
+
     }
 }
